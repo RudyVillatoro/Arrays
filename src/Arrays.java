21e0243975a345b20a6edf6c4f5bb23bebce7b52
Rudy <ruses40@hotmail.com>
2020-02-05 14:56:40 -0600

    Public Class

---
+++ src/Arrays.java
@@ -0,0 +1,6 @@
+public class Arrays {
+    public static void main(String[] args){
+        System.out.println("I'm watched by git.");
+        System.out.println("I'm watched by git 2.");
+    }
+}
